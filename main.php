72e7133da948060bcc357672dc53382db1804ee2
Rendyll Ryan Cabardo <rendyllcabardo11@gmail.com>
2024-10-09 17:36:00 +0800

    Update main.php

--- main.php
+++ main.php
@@ -6,8 +6,6 @@ $newConnection->editProduct();
 $newConnection->deleteProduct();
 $products = [];
 
-
-
 if (isset($_POST['filterProducts'])) {
     $selectedCategory = $_POST['selectedCategory'];
     $startDate = $_POST['startDate'];
@@ -78,16 +76,17 @@ if (isset($_POST['filterProducts'])) {
                     <button class="btn btn-primary" type="submit" name="searchbutton">Search</button>
                     <button class="btn btn-warning ms-3" type="button"
                         onclick="window.location.href='main.php'">Reload</button>
-
                 </form>
             </div>
         </div>
     </nav>
+
     <form action="" method="POST">
         <button class="btn btn-success me-2" type="submit" name="instock">In Stock</button>
         <button class="btn btn-danger" type="submit" name="outofstock">Out of Stock</button>
     </form>
     <hr class="mb-4">
+
     <!-- TABLE -->
     <div class="table-responsive">
         <table class="table table-hover">
@@ -135,89 +134,9 @@ if (isset($_POST['filterProducts'])) {
         </table>
     </div>
 
-    <!-- ADD MODAL -->
-    <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header bg-success">
-                    <h5 class="modal-title" id="exampleModalLabel">ADD PRODUCT</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="container-fluid">
-                        <form method="POST" action="">
-                            <div class="row g-3 mb-3">
-                                <div class="col">
-                                    <label for="inputproductname" class="form-label">Product Name</label>
-                                    <input type="text" class="form-control" id="inputproductname" name="productname"
-                                        required>
-                                </div>
-                            </div>
-                            <div class="row g-3 mb-4">
-                                <div class="col">
-                                    <label for="inputState" class="form-label">Category</label>
-                                    <select id="inputState" class="form-select" name="category" required>
-                                        <option selected disabled>Select Category</option>
-                                        <option value="Vegetables">Vegetables</option>
-                                        <option value="Fruits">Fruits</option>
-                                        <option value="Drinks">Drinks</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="row g-3 mb-4">
-                                <div class="col">
-                                    <label for="inputQuantity" class="form-label">Quantity</label>
-                                    <input type="number" class="form-control" id="inputQuantity" name="quantity"
-                                        required>
-                                </div>
-                                <div class="col">
-                                    <label for="inputDate" class="form-label">Purchased Date</label>
-                                    <input type="date" class="form-control" id="inputDate" name="purchasedate" required>
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-success" name="addproduct">Add Product</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php include 'addmodal.php'; ?>
+    <?php include 'filtermodal.php'; ?>
 
-    <!-- FILTER MODAL -->
-    <div class="modal fade" id="filterModal" tabindex="-1" aria-labelledby="filterModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header bg-info">
-                    <h5 class="modal-title" id="filterModalLabel">Filter Products</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form method="POST">
-                        <div class="mb-3">
-                            <label class="form-label">Choose a Category</label>
-                            <select class="form-select" name="selectedCategory" required>
-                                <option selected disabled>Select Category</option>
-                                <option value="Vegetables">Vegetables</option>
-                                <option value="Fruits">Fruits</option>
-                                <option value="Drinks">Drinks</option>
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Purchased Date Range</label>
-                            <div class="d-flex">
-                                <input type="date" class="form-control me-2" name="startDate">
-                                <input type="date" class="form-control" name="endDate">
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-info" name="filterProducts">Filter</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <!-- Include the Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
