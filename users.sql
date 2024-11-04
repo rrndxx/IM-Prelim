bd662d6fa45a32898faa1f053bf3824143f24335
Rendyll Ryan Cabardo <rendyllcabardo11@gmail.com>
2024-11-04 17:22:05 +0800

    Add files via upload

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 30, 2024 at 10:26 AM
+-- Generation Time: Nov 04, 2024 at 10:21 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -29,16 +29,25 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `birthdate` date NOT NULL,
+  `gender` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `password` varchar(100) NOT NULL,
+  `role` varchar(100) NOT NULL,
+  `date_created` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `address`, `birthdate`, `gender`, `username`, `password`, `role`, `date_created`) VALUES
+(1, 'Roxanne', 'Mercader', 'Polambato', '2024-10-31', 'Female', 'admin', 'admin', 'Customer', '2024-11-04'),
+(2, 'Ongbak', 'Padayugyog', 'Thailand', '2024-11-04', 'Male', 'admin2', 'admin2', 'Customer', '2024-11-04'),
+(3, 'asd', 'ads', 'ads', '2024-11-09', 'Male', '123', '123', 'Customer', '2024-11-04');
 
 --
 -- Indexes for dumped tables
@@ -58,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
