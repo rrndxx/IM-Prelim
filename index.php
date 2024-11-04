58fa5a8c3061b4e04a4bd5ad5b04a9c759ba87b3
Rendyll Ryan Cabardo <rendyllcabardo11@gmail.com>
2024-11-04 17:20:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,17 +13,13 @@ if (isset($_POST['login'])) {
 
     if ($user) {
         if ($user->password === $password) {
-            $_SESSION['user'] = $username;
+            $_SESSION['user'] = $user->first_name;
             header('Location: main.php');
             exit;
-        } else {
-            header('Location: index.php');
-            exit;
         }
-    } else {
-        header('Location: index.php');
-        exit;
     }
+    header('Location: index.php');
+    exit;
 }
 ?>
 
@@ -34,7 +30,8 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700&display=swap" rel="stylesheet">
@@ -47,56 +44,42 @@ if (isset($_POST['login'])) {
 
         .container {
             box-shadow: rgba(0, 0, 0, 0.44) 0px 3px 8px;
+            border-radius: 20px;
         }
 
-        .modal {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: fixed;
-            z-index: 1;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            overflow: auto;
-            background-color: rgba(0, 0, 0, 0.5);
-        }
-
-        .modal-content {
-            background-color: #fff;
-            padding: 20px;
-            border: 1px solid #888;
-            width: 300px;
-        }
-
-        .close {
-            color: #aaa;
-            float: right;
-            font-size: 28px;
-            font-weight: bold;
+        .register-link {
+            position: absolute;
+            top: 20px;
+            right: 20px;
+            z-index: 1000;
         }
     </style>
 </head>
 
 <body>
-    <div class="container bg-light p-4 text-start" style="height: 350px; width: 400px;">
-        <h2 class="mt-4 text-center mb-3">Login</h2>
-        <form action="index.php" method="POST" class="mt-3">
-            <div class="form-group">
-                <label for="username" class="form-label">Username</label>
-                <input type="text" class="form-control" name="username" required>
-            </div>
-            <div class="form-group mt-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" name="password" required>
-            </div>
-            <div class="text-center mt-3">
-                <button type="submit" class="btn btn-success mt-4" name="login">Login</button>
-            </div>
-        </form>
+    <a href="register.php" class="register-link btn btn-warning">Register</a>
+    <div class="d-flex justify-content-center">
+        <div class="container bg-light p-4 text-start" style="height: 350px; width: 400px;">
+            <h2 class="mt-4 text-center mb-3">Login</h2>
+            <form action="index.php" method="POST" class="mt-3">
+                <div class="form-group">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" name="username" required>
+                </div>
+                <div class="form-group mt-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password" required>
+                </div>
+                <div class="text-center mt-3">
+                    <button type="submit" class="btn btn-success mt-4" name="login">Login</button>
+                </div>
+            </form>
+        </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
